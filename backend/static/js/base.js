02b33d98e1250f45a2f8022d085f76e3be75eddb
Felipe Salviano <felipesalviano00@hotmail.com>
2025-05-22 16:49:08 -0300

    FINISH: Concertado algumas func, css melhorado, footer e cache desenvolvido

--- backend/static/js/base.js
+++ backend/static/js/base.js
@@ -2,3 +2,14 @@ document.querySelector('.menu-toggle').addEventListener('click', function() {
     const menu = document.querySelector('.navegacao ul');
     menu.classList.toggle('active');
 });
+
+function copiarPix() {
+    const chavePix = "361a3c9c-9099-40f9-81f6-3e274bac3de0";  // Insira sua chave real
+    navigator.clipboard.writeText(chavePix)
+      .then(() => {
+        alert("✅ Chave Pix copiada com sucesso!");
+      })
+      .catch(err => {
+        console.error('Erro ao copiar a chave Pix:', err);
+      });
+  }
